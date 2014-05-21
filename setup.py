eaeff79c40df8cd86dbb0a485362d7a5d8a3fcfb
ericchiang <eric.chiang.m@gmail.com>
2014-05-21 15:01:59 -0400

    pypi cleanup

--- setup.py
+++ setup.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
-from setuptools import find_packages
 
 setup(name='fprint',
+      packages=['fprint'],
       version='1.0',
       description='Fancy printing',
       author='Eric Chiang',
       author_email='eric.chiang.m@gmail.com',
       url='http://github.com/ericchiang/fprint',
-      packages=find_packages(),
-      license='MIT'
+      classifiers=[],
      )
